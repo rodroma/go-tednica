e8271608748949a0f5894e2637d13215d0baa331
rodroma <rodrigoleonardomartin@gmail.com>
2021-02-08 18:57:19 -0300

    parallel by hand

--- internal/get_item_by_id.go
+++ internal/get_item_by_id.go
@@ -2,6 +2,7 @@ package internal
 
 import (
 	"context"
+	"sync"
 )
 
 type GetItemByIDResponse struct {
@@ -19,19 +20,49 @@ type GetItemByID struct{
 }
 
 func (uc GetItemByID) GetItemByID(ctx context.Context, id string) (GetItemByIDResponse, error) {
-	item, err := uc.ItemGetter.GetItem(ctx, id)
+	ctx, cancelFunc := context.WithCancel(ctx)
+	defer cancelFunc()
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	var err error
+
+	var item Item
+	go func(wg *sync.WaitGroup) {
+		defer wg.Done()
+		item, err = uc.ItemGetter.GetItem(ctx, id)
+
+		if err != nil {
+			cancelFunc()
+		}
+	}(&wg)
+
+	var salePrice *SalePrice
+	go func(wg *sync.WaitGroup) {
+		defer wg.Done()
+
+		sp, err := uc.SalePriceGetter.GetSalePrice(ctx, id)
+
+		if err != nil {
+			// Do stuff, log maybe?
+			return
+		}
+
+		salePrice = &sp
+	}(&wg)
+
+	wg.Wait()
+
 	if err != nil {
 		return GetItemByIDResponse{}, err
 	}
+
 	response := GetItemByIDResponse{Item: item}
-
-	salePrice, err := uc.SalePriceGetter.GetSalePrice(ctx, id)
-	if err != nil {
-		return response, nil
+	if salePrice != nil {
+		response.Price = salePrice.Price
+		response.OriginalPrice = salePrice.RegularPrice
 	}
-
-	response.OriginalPrice = salePrice.RegularPrice
-	response.Price = salePrice.Price
 
 	return response, nil
 }
